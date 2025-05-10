2dd3793f91c1c2b5962c80a8d315a6670b97846e
WhoamiI00 <ankitisop12@gmail.com>
2025-05-10 20:02:36 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,16 +4,16 @@ export default function Footer() {
   return (
     <footer className='py-4'>
       <div className='container flex flex-col items-center justify-between gap-x-3 gap-y-1 text-center text-sm text-muted-foreground sm:flex-row'>
-        <p>RealChat &copy;{new Date().getFullYear()}. All rights reserved.</p>
+        <p>NeoChat &copy;{new Date().getFullYear()}. All rights reserved.</p>
         <p className='text-xs'>
           Developed by{' '}
           <Link
             target='_blank'
             rel='noopener noreferrer'
             className='text-emerald-500 transition-colors hover:text-emerald-500'
-            href='https://www.hamedbahram.io/'
+            href='https://github.com/WhoamiI00'
           >
-            Studio H
+            Nexus
           </Link>
         </p>
       </div>
